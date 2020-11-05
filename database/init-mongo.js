7a5a65a1c745ddd20c26c5102ded251b93d725c3
Sebastian Barcelona <sbarcelona@gmail.com>
2020-11-05 13:58:51 -0300

    change credentials on init-mongo

--- database/init-mongo.js
+++ database/init-mongo.js
@@ -4,8 +4,8 @@ for (let i = databases.length - 1; i >= 0; i--) {
     db = db.getSiblingDB(databases[i])
 
     db.createUser({
-        user: "kreitech",
-        pwd: "kreitech",
+        user: "agesic",
+        pwd: "agesic-control-versiones",
         roles: [{
             role: "readWrite",
             db: databases[i]
